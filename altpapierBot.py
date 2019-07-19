904aadea8b95280fdacb2d5a7345bc253e5bc966
Daniel Gnägi <daniel.gnaegi@digitecgalaxus.ch>
2019-07-19 23:13:53 +0200

    move config to jsonFile

--- altpapierBot.py
+++ altpapierBot.py
@@ -1,18 +1,23 @@
-import telegram
 from datetime import datetime
 from datetime import date
 from datetime import timedelta
 from helper.wasteDisposal import wasteDisposal
+from helper.bot import bot
 import json
 
-zip = "doNotCommit"
-chatId = doNotCommit
-bot = telegram.Bot(token='doNotCommit')
+with open('config.json') as data_file:    
+    data = json.load(data_file)
+    zip = data["zip"]
+    chatId = data["chatId2"]
+    token = data["token"]
 
+bot = bot(token)
 disposals = [wasteDisposal("paper", zip), wasteDisposal("cardboard", zip)]
 
 for disposal in disposals:
     if disposal.GetNextDisposalDate() == date.today() and datetime.now().hour < 12:
-      print(disposal.GetName + "disposal is today!")
+      bot.SendMessage(chatId, disposal.GetName + "disposal is today!")
     if (disposal.GetNextDisposalDate() - timedelta(days=1)).date() == date.today() and datetime.now().hour > 12:
-      print (disposal.GetName + "disposal will be tomorrow!")   
+      bot.SendMessage(chatId, disposal.GetName + "disposal will be tomorrow!")   
+    else:
+      bot.SendMessage(chatId, "No Disposal!")
