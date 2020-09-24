9b017d431e67eb2fcc6839b28f5d4edc5b7ebd93
Daniel Gnaegi <mail@dgnaegi.ch>
2020-09-24 18:50:10 +0200

    add exception handling

--- altpapierNotifier.py
+++ altpapierNotifier.py
@@ -4,6 +4,7 @@ from helper.stgallenApi import stgallenApi
 from dataStorage.dataAccess import dataAccess
 from helper.bot import bot
 import json
+import sys
 
 with open('config.json') as data_file:    
     data = json.load(data_file)
@@ -23,18 +24,21 @@ todayCardboardZHDisposalZipCodes = zurichApi.GetTodayPaperDisposalZipCodes()
 sentMessageZurich = 0
 
 for userDataSet in zhUserDataSets:
-  if userDataSet.zipCode in tomorrowZHPaperDisposalZipCodes and datetime.now().hour > 12:
-    bot.SendMessage(userDataSet.chatId, "Paper disposal will be tomorrow!")
-    sentMessageZurich = sentMessageZurich + 1
-  if userDataSet.zipCode in todayZHPaperDisposalZipCodes and datetime.now().hour < 12:
-    bot.SendMessage(userDataSet.chatId, "Paper disposal is today!") 
-    sentMessageZurich = sentMessageZurich + 1
-  if userDataSet.zipCode in tomorrowZHCardboardDisposalZipCodes and datetime.now().hour > 12:
-    bot.SendMessage(userDataSet.chatId, "Cardboard disposal will be tomorrow!")
-    sentMessageZurich = sentMessageZurich + 1
-  if userDataSet.zipCode in todayCardboardZHDisposalZipCodes and datetime.now().hour < 12:
-    bot.SendMessage(userDataSet.chatId, "Cardboard disposal is today!") 
-    sentMessageZurich = sentMessageZurich + 1
+  try:  
+    if userDataSet.zipCode in tomorrowZHPaperDisposalZipCodes and datetime.now().hour > 12:
+      bot.SendMessage(userDataSet.chatId, "Paper disposal will be tomorrow!")
+      sentMessageZurich = sentMessageZurich + 1
+    if userDataSet.zipCode in todayZHPaperDisposalZipCodes and datetime.now().hour < 12:
+      bot.SendMessage(userDataSet.chatId, "Paper disposal is today!") 
+      sentMessageZurich = sentMessageZurich + 1
+    if userDataSet.zipCode in tomorrowZHCardboardDisposalZipCodes and datetime.now().hour > 12:
+      bot.SendMessage(userDataSet.chatId, "Cardboard disposal will be tomorrow!")
+      sentMessageZurich = sentMessageZurich + 1
+    if userDataSet.zipCode in todayCardboardZHDisposalZipCodes and datetime.now().hour < 12:
+      bot.SendMessage(userDataSet.chatId, "Cardboard disposal is today!") 
+      sentMessageZurich = sentMessageZurich + 1
+  except:
+    logf.write("An exception occurred for ZIP: " + str(userDataSet.zipCode) + " " + str(sys.exc_info()[1]) + "\r\n")
 
 sgUserDataSets = dataAccess.getStGallenUserdata()
 
@@ -47,18 +51,21 @@ todaySGCardboardDisposalAreaCodes = stgallenApi.GetTodayCardboardDisposalAreaCod
 sentMessageStGallen = 0
 
 for userDataSet in sgUserDataSets:
-  if userDataSet.areaCode in tomorrowSGPaperDisposalAreaCodes and datetime.now().hour > 12:
-    bot.SendMessage(userDataSet.chatId, "Paper disposal will be tomorrow!")
-    sentMessageStGallen = sentMessageStGallen + 1
-  if userDataSet.areaCode in todaySGPaperDisposalAreaCodes and datetime.now().hour < 12:
-    bot.SendMessage(userDataSet.chatId, "Paper disposal is today!") 
-    sentMessageStGallen = sentMessageStGallen + 1
-  if userDataSet.areaCode in tomorrowSGCardboardDisposalAreaCodes and datetime.now().hour > 12:
-    bot.SendMessage(userDataSet.chatId, "Cardboard disposal will be tomorrow!")
-    sentMessageStGallen = sentMessageStGallen + 1
-  if userDataSet.areaCode in todaySGCardboardDisposalAreaCodes and datetime.now().hour < 12:
-    bot.SendMessage(userDataSet.chatId, "Cardboard disposal is today!") 
-    sentMessageStGallen = sentMessageStGallen + 1
+  try:
+    if userDataSet.areaCode in tomorrowSGPaperDisposalAreaCodes and datetime.now().hour > 12:
+      bot.SendMessage(userDataSet.chatId, "Paper disposal will be tomorrow!")
+      sentMessageStGallen = sentMessageStGallen + 1
+    if userDataSet.areaCode in todaySGPaperDisposalAreaCodes and datetime.now().hour < 12:
+      bot.SendMessage(userDataSet.chatId, "Paper disposal is today!") 
+      sentMessageStGallen = sentMessageStGallen + 1
+    if userDataSet.areaCode in tomorrowSGCardboardDisposalAreaCodes and datetime.now().hour > 12:
+      bot.SendMessage(userDataSet.chatId, "Cardboard disposal will be tomorrow!")
+      sentMessageStGallen = sentMessageStGallen + 1
+    if userDataSet.areaCode in todaySGCardboardDisposalAreaCodes and datetime.now().hour < 12:
+      bot.SendMessage(userDataSet.chatId, "Cardboard disposal is today!") 
+      sentMessageStGallen = sentMessageStGallen + 1
+  except:
+    logf.write("An exception occurred for Area: " + str(userDataSet.area) + " " + str(sys.exc_info()[1]) + "\r\n")
 
 sendLog = open("send.log", "w")
 sendLog.write("Sent messages Zurich: " + str(sentMessageZurich) + "\r\n")
