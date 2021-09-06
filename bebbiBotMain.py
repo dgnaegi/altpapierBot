e771eb5261e25f367d4eee8572aec364beb27a62
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2021-09-06 22:19:49 +0200

    Improve Basel dialect
    This commit improves the language by following the holy Baseldeutsch-Wörterbuch (Rudolf Suter, CM Verlag).

--- bebbiBotMain.py
+++ bebbiBotMain.py
@@ -6,34 +6,34 @@ from helper.areaCodeParser import areaCodeBaselParser
 import json
     
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Sali, ich bi dr bebbi papier bot. Ich bi do zum di für d Karton- und Papierentsorgig in Basel z erinnere. Für meh Informatione, druck uf /hilf")
-    update.message.reply_text("Sag mir bitte weli Entsorgigszone (A-H) für di zuetrifft")
-    update.message.reply_text("bspw.: /area X")
+    update.message.reply_text("Sali, ych bi dr «Bebbi Bapyyr Bot». Ych bi doo, zem di an d Karton- und Bapyyrentsorgig in Baasel z erinnere. Fir mee Informazioone, drugg uf /hilf")
+    update.message.reply_text("Saag mir bitte, weeli Entsorgigszoone (A-H) fir di zuedrifft")
+    update.message.reply_text("Bsp.: /area X")
 
 def stop(update: Update, context: CallbackContext):
     chatId = update.effective_chat.id
     dataAccess.deleteUserData(chatId)
-    update.message.reply_text("Freut mi, dassi dir ha könne hälfe. Dini Date sind gänzlich glöscht worde. Bi witerhin do wenn wieder mini hilf bruuchsch <3")
+    update.message.reply_text("Frait mi, dass ych dir ha kenne hälfe. Dyyni Daate sind gänzlig glescht worde. Ych bi wyterhin doo, wenn wider myyni Hilf bruuchsch <3")
 
 def help(update: Update, context: CallbackContext):
-    update.message.reply_text("Meh Informatione sowie au e kleins Tutorial findsch uf https://dgnaegi.ch/2020/09/06/altpapierbot/. Für witeri Frooge kasch au dr Entwickler diräkt kontaktiere: @ignobled")
+    update.message.reply_text("Mee Informazioone sowie au e glains Tutorial findsch uf https://dgnaegi.ch/2020/09/06/altpapierbot/. Fir wyteri Frooge kasch au dr Entwiggler diräggt aaschryybe: @ignobled")
 
 def area(update: Update, context: CallbackContext):
     areaCodeOrNone = areaCodeBaselParser.Parse(context.args)
     chatId = update.message.chat_id
 
     if areaCodeOrNone is None:
-        update.message.reply_text("Ungültigi igob! Bitte gib e gültigi Entsorgigszone im Ruum Basel (A-H) in dem Format ii:")
+        update.message.reply_text("Ungiltig Yygob! Bitte gib e giltig Entsorgigszoone im Ruum Baasel (A-H) in däm Formaat yy:")
         update.message.reply_text("/area X")
         return
     
     data = userData(chatId, None, areaCodeOrNone)
     success = dataAccess.saveUserData(data)
     if success:
-        update.message.reply_text("Du bisch igrichtet und wirdsch ab jetzt benochrichtigt sobald Karton- und Papierentsorgig in dinere Zone stattfindet.")
-        update.message.reply_text("Du kasch di vo de Benochrichtige abmelde indem du /stop itiippsch")
+        update.message.reply_text("Du bisch yygrichtet und wirsch ab jetz benoochrichtigt, sobald e Karton- und Bapyyrentsorgig in dyynere Zoone stattfindet.")
+        update.message.reply_text("Du kasch di vo de Benoochrichtige abmälde, indäm du /stop yytippsch")
     else:
-        update.message.reply_text("Du hesch di scho agmäldet! Bitte mäld di doch zersch ab, in dem du /stop i tippsch")
+        update.message.reply_text("Du hesch di scho aagmolde! Bitte mäld di doch zersch ab, indäm du /stop yytippsch")
 
 def main():
     with open('config.json') as data_file:    
