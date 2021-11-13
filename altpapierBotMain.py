7cf2a302970bc837376e4f32f2e5f05ad29d1a5e
Daniel Gnaegi <mail@dgnaegi.ch>
2021-11-13 09:41:14 +0100

    remove st.gallen registration, replaced by sanktgallenbot

--- altpapierBotMain.py
+++ altpapierBotMain.py
@@ -10,8 +10,6 @@ def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hi, im altpapierBot. Here to remind you about cardboard and paper removal for Zurich or St.Gallen. For more information enter /help")
     update.message.reply_text("For Zurich, please let me know your postal code by sending it in this format:")
     update.message.reply_text("/zip 80XX")
-    update.message.reply_text("For St.Gallen, send your disposal area (A,B,C,...) like this:")
-    update.message.reply_text("/area X")
 
 def stop(update: Update, context: CallbackContext):
     chatId = update.message.chat_id
@@ -38,24 +36,6 @@ def zip(update: Update, context: CallbackContext):
     else:
         update.message.reply_text("You are already subscribed! Please unsubscribe first by sending /stop")
 
-def area(update: Update, context: CallbackContext):
-    areaCodeOrNone = areaCodeParser.Parse(context.args)
-    chatId = update.message.chat_id
-
-    if areaCodeOrNone is None:
-        update.message.reply_text("Invalid input! Please enter a valid disposal area code of the city of St.Gallen in this format:")
-        update.message.reply_text("/area X")
-        update.message.reply_text("Valid areas are: A, B, C, D, E, F, G, H, I, J, K, L-OST, L-WEST")
-        return
-
-    data = userData(chatId, None, areaCodeOrNone)
-    success = dataAccess.saveUserData(data)
-    if success:
-        update.message.reply_text("You're setup and will be notified from now on for disposal in area {}.".format(areaCodeOrNone))
-        update.message.reply_text("You can unsubscribe by sending /stop")
-    else:
-        update.message.reply_text("You are already subscribed! Please unsubscribe first by sending /stop")
-
 def stats(update: Update, context: CallbackContext):
     zhUserCount = "Users in Zurich: " + str(len(dataAccess.getZurichUserdata()))
     sgUserCount = "Users in St.Gallen: " + str(len(dataAccess.getStGallenUserdata()))
@@ -76,7 +56,6 @@ def main():
     dp.add_handler(CommandHandler('stats', stats))
     dp.add_handler(CommandHandler('zip',zip, pass_args=True))
     dp.add_handler(CommandHandler('postalcode',zip, pass_args=True))
-    dp.add_handler(CommandHandler('area',area, pass_args=True))
     updater.start_polling()
     updater.idle()
 
